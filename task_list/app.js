b65ea366916774c746e4fb3898aaca8bf115d561
tanpham <phamtan500@gmail.com>
2018-06-23 10:10:01 +0700

    event handle

--- task_list/app.js
+++ task_list/app.js
@@ -12,12 +12,69 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // clear task event
+  clearBtn.addEventListener('click', clearTasks);
+
+  // filter tasks
+  filter.addEventListener('keyup',filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-  console.log(taskInput.value)
   if(taskInput.value === '') {
     alert('Add a task');
+    return
   }
+
+  // create li
+  const li = document.createElement('li');
+  // add class
+  li.className = 'collection-item';
+  // create text node
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create new link
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class = "fa fa-remove"></i>';
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Remove task
+function removeTask(e){
+  if(e.target.classList.contains('fa-remove')) {
+    if(confirm('Are You Sure?')){
+      e.target.parentElement.parentElement.remove();
+    }    
+  }
+}
+
+function clearTasks(e){
+  // taskList.innerHTML = '';
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+
+}
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    }else{
+      task.style.display = 'none';
+    }
+  });
 }
